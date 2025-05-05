0f5badf8a571ebc729321365300ff84cb3f2e507
Carlos Morales <baa4tsdev@gmail.com>
2025-05-05 03:59:42 +0200

    Append examples ./api/ejemplos:

--- api/index.php
+++ api/index.php
@@ -1,11 +1,3 @@
 <?php
-$extensiones = get_loaded_extensions();
-sort($extensiones);
-
-echo "Extensiones de PHP cargadas:\n\n";
-
-foreach ($extensiones as $extension) {
-    echo "- $extension\n";
-}
-
-echo "\nTotal: " . count($extensiones) . " extensiones.\n";
+require_once "./ejemplos/db.php";
+require_once "./ejemplos/extensiones.php";
